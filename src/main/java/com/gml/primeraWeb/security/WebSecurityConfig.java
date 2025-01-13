55ce756ca91a12635df18d08f361fef05c569ee9
Bruzzone Marcelo Daniel <bruzzone1804@gmail.com>
2025-01-13 02:22:52 -0300

    seguridad jdbc implementada

--- src/main/java/com/gml/primeraWeb/security/WebSecurityConfig.java
+++ src/main/java/com/gml/primeraWeb/security/WebSecurityConfig.java
@@ -1,39 +1,30 @@
 package com.gml.primeraWeb.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder builder) throws Exception {
+        builder.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username =?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?");
     }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails user1 = User.builder()
-                .username("user")
-                .password("{bcrypt}$2a$10$f5u7OO4MmyogoeRIRJJ0rOoEEAQojalBkGD2RClVcqMS1kDmxTMJK")
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User.builder()
-                .username("admin")
-                .password("{bcrypt}$2a$10$f5u7OO4MmyogoeRIRJJ0rOoEEAQojalBkGD2RClVcqMS1kDmxTMJK")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
-    }
-
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(
